32543e04129d5f53ab229328115f1f55bbabf20e
Maykel Santos <eumaykelandre@gmail.com>
2023-01-14 03:30:51 -0300

    primeiro_grafico

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# API LINK -> https://servicodados.ibge.gov.br/api/docs
+
 import requests
 import pandas as pd
 import plotly.express as px
@@ -7,4 +9,12 @@ url = 'https://servicodados.ibge.gov.br/api/v3/agregados/6784/periodos/2010|2011
 requisicao = requests.get(url)
 dados = requisicao.json()
 
-display(dados)
+pib = dados[0]['resultados'][0]['series'][0]['serie']
+df =pd.DataFrame.from_dict(pib, orient = 'index', columns = ['PIB'])
+df.reset_index(inplace = True)
+df.columns = ['Ano', 'PIB']
+df['PIB'] = df['PIB'].astype('int')
+pais = dados[0]['resultados'][0]['series'][0]['localidade']['nome']
+
+graphic = px.line(df, x = 'Ano', y = 'PIB', title= 'Timeline do PIB {}'.format(pais))
+graphic.show()
